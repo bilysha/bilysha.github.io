7b55566b8743534733b403f5c34bc283377a715c
Ilya Belavusau <ilya.belavusau@kronos.com>
2018-10-22 13:39:14 +0300

    add reactive movies application

---
+++ srcs/reactive-movies/precache-manifest.39276ee4d31fa978d7a1e30462e4d296.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "bec2f5ab8ee6ebd78354",
+    "url": "/static/css/main.960891aa.chunk.css"
+  },
+  {
+    "revision": "abd0b73a9d26d36ada1c",
+    "url": "/static/js/1.abd0b73a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "bec2f5ab8ee6ebd78354",
+    "url": "/static/js/main.bec2f5ab.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "2a918283ea5f5509b7ded30aa6cacb9b",
+    "url": "/index.html"
+  }
+];
